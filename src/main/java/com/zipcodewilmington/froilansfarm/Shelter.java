8a0b781d5993e72bb2e38e704832258be8a5c383
Ryan Chudd <ryan.a.chudd@gmail.com>
2022-07-20 20:55:12 -0400

    shelter and horsie setup

---
+++ src/main/java/com/zipcodewilmington/froilansfarm/Shelter.java
@@ -0,0 +1,28 @@
+package com.zipcodewilmington.froilansfarm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Shelter<CreatureType> {
+    List<CreatureType> creatureList;
+
+    public void add(CreatureType creatureType) {
+        if (creatureList == null) {
+            creatureList = new ArrayList<CreatureType>();
+        }
+        creatureList.add(creatureType);
+    }
+
+    public Integer getNumberOfCreature() {
+        if (creatureList == null) return 0;
+        return creatureList.size();
+    }
+
+    public void remove(CreatureType creatureType) { // could throw exception
+        if (creatureList != null) creatureList.remove(creatureType);
+    }
+
+    public void clear() {
+        if (creatureList != null) creatureList.clear();
+    }
+}
