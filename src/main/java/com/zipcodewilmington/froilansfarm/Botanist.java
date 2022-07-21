2cbf7bd6ae964ce6302c4da32160336373c729e5
eugeneshine1 <eugeneshine1@gmail.com>
2022-07-20 22:21:21 -0400

    farmer and botanist

--- src/main/java/com/zipcodewilmington/froilansfarm/Botanist.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Botanist.java
@@ -2,5 +2,11 @@ package com.zipcodewilmington.froilansfarm;
 
 public abstract class Botanist extends Person{
 
-    void plant(Crop crop, CropRow cropRow);
+    public Botanist(String name) {
+        super(name);
+    }
+
+    void plant(Crop crop, CropRow cropRow) {
+
+    }
 }
