cbcb238bba0db684364a4a0894122c22bde258b6
Ryan Chudd <ryan.a.chudd@gmail.com>
2022-07-21 15:50:28 -0400

    getInstance of produce store fixed

--- src/main/java/Produce/ProduceStore.java
+++ src/main/java/Produce/ProduceStore.java
@@ -3,34 +3,43 @@ package Produce;
 import java.util.HashMap;
 import java.util.Map;
 
-public class ProduceStore<Produce> {
+public class ProduceStore {
 
-    private final  ProduceStore<Produce> instance = new ProduceStore<Produce>();
-    public Map<Produce, Integer> foodStore;
+    private static final ProduceStore instance = new ProduceStore();
+    public Map<Produce<Edible>, Integer> foodStore;
 
     ProduceStore(){
         this.foodStore = new HashMap<>();
-
     }
 
-    public ProduceStore<Produce> getInstance() {
+    public static ProduceStore getInstance() {
         return instance;
     }
 
-    public void add(Produce edible, Integer numberOfProduce){
-        this.foodStore.put(edible, numberOfProduce);
+    public void add(Produce<Edible> edible, Integer numberOfProduce){
+        // check if produce in the keyset
+        if (foodStore.containsKey(edible)) {
+            foodStore.put(edible, numberOfProduce + foodStore.get(edible));
+        } else {
+            // if not, put like below
+            foodStore.put(edible, numberOfProduce);
+        }
     }
 
-    public void remove(Produce edible) {
+    public void add(Produce<Edible> edibleProduce){
+        add(edibleProduce, 1);
+    }
+
+    public void remove(Produce<Edible> edible) {
         this.foodStore.remove(edible);
     }
 
-    public Integer getQuantityInStorage(Produce edible) {
+    public Integer getQuantityInStorage(Produce<Edible> edible) {
 
         return this.foodStore.get(edible);
     }
 
-    public Boolean contains(Produce edible){
+    public Boolean contains(Produce<Edible> edible){
         return this.foodStore.containsKey(edible);
     }
 
