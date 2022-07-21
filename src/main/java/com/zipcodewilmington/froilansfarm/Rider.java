f833693d451a1ffeabe18ec84e66d8b257849870
eugeneshine1 <eugeneshine1@gmail.com>
2022-07-21 15:19:49 -0400

    added rider and deleted edible

--- src/main/java/com/zipcodewilmington/froilansfarm/Rider.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Rider.java
@@ -2,6 +2,9 @@ package com.zipcodewilmington.froilansfarm;
 
 public abstract class Rider <RiderType extends Person> {
     RiderType rider;
+    public Rider () {
+        this.rider = null;
+    }
     public Rider (RiderType rider) {
         this.rider = rider;
 
