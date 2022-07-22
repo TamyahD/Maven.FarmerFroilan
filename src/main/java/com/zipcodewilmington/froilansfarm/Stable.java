cb2159a69fd3bdbc2688015e0d930552f96fa1ad
Ryan Chudd <ryan.a.chudd@gmail.com>
2022-07-22 12:14:25 -0400

    more each day tests

--- src/main/java/com/zipcodewilmington/froilansfarm/Stable.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Stable.java
@@ -5,4 +5,12 @@ import java.util.Iterator;
 
 
 public class Stable extends Shelter<Horse> {
+    private final int maxNumberOfHorses = 4;
+    public Stable () {
+        Farm.getInstance().addStable(this);
+    }
+
+    public int getMaxNumberOfHorses() {
+        return maxNumberOfHorses;
+    }
 }
--- src/test/java/com/zipcodewilmington/froilansfarm/EachDayTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/EachDayTest.java
@@ -1,6 +1,7 @@
 package com.zipcodewilmington.froilansfarm;
 
 import Produce.ProduceStore;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class EachDayTest {
@@ -15,14 +16,63 @@ public class EachDayTest {
             Froilanda eats 2 EarCorn, 1 Tomoato, and 2 Egg.
          */
         //Froilan and Froilanda will be eager initialized
-        for (Stable stable : Farm.getInstance().getStableList()) {
-            for (int i = 0; i < stable.getNumberOfCreature(); i++) {
-                Froilan.getFroilan().mount(stable.get(i));
-                Froilan.getFroilan().dismount(stable.get(i));
-                Froilanda.getFroilanda().mount(stable.get(i));
-                Froilanda.getFroilanda().dismount(stable.get(i));
+
+        // eat a certain amount of produce in the store
+            // eat method removes the eidble from the store?
+
+        // make stable
+        Stable stabley = new Stable();
+        // put 10 horses in 3 stables
+        while (Farm.getInstance().getTotalHorseCount() < 10) {
+            Horse myHorse = new Horse();
+
+            for (int i = Farm.getInstance().getStableList().size()-1; i < Farm.getInstance().getStableList().size(); i++) {
+                if (Farm.getInstance().getStableList().get(i).size() < Farm.getInstance().getStableList().get(i).getMaxNumberOfHorses()) {
+                    Farm.getInstance().getStableList().get(i).add(myHorse);
+                    System.out.println(Farm.getInstance().getTotalHorseCount() + " horses");
+                    System.out.println("In " + Farm.getInstance().getStableList().size() + " stables");
+                    break;
+                } else {
+                    new Stable().add(myHorse);
+                    System.out.println(Farm.getInstance().getTotalHorseCount() + " horses");
+                    System.out.println("In " + Farm.getInstance().getStableList().size() + " stables");
+                    break;
+                }
             }
         }
-//        ProduceStore.getInstance().add(corn, 7);
+
+        // Assert that there are 3 stables
+        Integer expected = 10;
+        Assert.assertEquals(expected, Farm.getInstance().getTotalHorseCount());
+        Assert.assertEquals(3, Farm.getInstance().getStableList().size());
+
+        for (Stable stable : Farm.getInstance().getStableList()) {
+            for (int k = 0; k < stable.getNumberOfCreature(); k++) {
+                System.out.println("Yeehaw!");
+                Froilan.getFroilan().mount(stable.get(k));
+                Froilan.getFroilan().dismount(stable.get(k));
+                Froilanda.getFroilanda().mount(stable.get(k));
+                Froilanda.getFroilanda().dismount(stable.get(k));
+                for (int j = 0; j < 3; j++) {
+                    stable.get(k).eat(new EarOFCorn());
+                }
+            }
+        }
+
+        System.out.println(Froilan.getFroilan().makeNoise());
+        Froilan.getFroilan().eat(new EarOFCorn());
+        Froilan.getFroilan().eat(new Tomato());
+        Froilan.getFroilan().eat(new Tomato());
+        for (int k = 0; k < 5; k++) {
+            Froilan.getFroilan().eat(new Egg());
+        }
+        System.out.println(Froilan.getFroilan().makeNoise());
+
+        Froilanda.getFroilanda().eat(new EarOFCorn());
+        Froilanda.getFroilanda().eat(new EarOFCorn());
+        Froilanda.getFroilanda().eat(new Tomato());
+        Froilanda.getFroilanda().eat(new Egg());
+        Froilanda.getFroilanda().eat(new Egg());
+        System.out.println(Froilanda.getFroilanda().makeNoise());
     }
 }
