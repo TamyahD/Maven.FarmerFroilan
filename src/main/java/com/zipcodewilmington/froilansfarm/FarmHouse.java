b0ef1acb12afe78e3113c4d8f2b78456fafc40e8
Ryan Chudd <ryan.a.chudd@gmail.com>
2022-07-24 22:35:17 -0400

    checking farmhouse size

--- src/main/java/com/zipcodewilmington/froilansfarm/FarmHouse.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/FarmHouse.java
@@ -6,6 +6,8 @@ import java.util.Iterator;
 public class FarmHouse extends Shelter<Person> {
     public FarmHouse() {
         Farm.getInstance().setFarmHouse(this);
+        this.add(Froilan.getFroilan());
+        this.add(Froilanda.getFroilanda());
     }
     @Override
     public Iterator<Person> iterator() {
