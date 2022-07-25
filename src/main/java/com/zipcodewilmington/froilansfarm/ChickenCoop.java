db6a3bba2468ede5cbc35656c0492dbd92fe5bbe
Mike <mike@ZipcodersMBP41.fios-router.home>
2022-07-24 20:38:52 -0400

    implemented iterable in shelter for coops n stables

--- src/main/java/com/zipcodewilmington/froilansfarm/ChickenCoop.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/ChickenCoop.java
@@ -2,6 +2,23 @@ package com.zipcodewilmington.froilansfarm;
 
 import java.util.Iterator;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class ChickenCoop extends Shelter<Chicken>{
+
+    private final int maxNumberOfChickens = 5;
+
+    public ChickenCoop() {
+        Farm.getInstance().addChickenCoop(this);
+    }
+
+    @Override
+    public Iterator<Chicken> iterator() {
+        return creatureList.iterator();
+    }
+
+    @Override
+    public void forEach(Consumer<? super Chicken> action) {
+        super.forEach(action);
+    }
 }
---
+++ src/test/java/com/zipcodewilmington/froilansfarm/MondayTest.java
@@ -0,0 +1,58 @@
+package com.zipcodewilmington.froilansfarm;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MondayTest {
+
+    @Test
+    public void rideHorsesStable1() {
+
+        Stable stable1 = new Stable();
+
+        stable1.add(new Horse());
+        stable1.add(new Horse());
+        stable1.add(new Horse());
+
+        for (Horse h : stable1) {
+            Froilan.getFroilan().mount(h);
+            System.out.println("YEEHAW!");
+            Froilan.getFroilan().dismount(h);
+        }
+
+        Assert.assertTrue(stable1.get(0) instanceof Rideable);
+    }
+
+    @Test
+    public void rideHorsesStableTwo() {
+
+        Stable stable2 = new Stable();
+
+        for (int i = 0; i < 3; i++) {
+            stable2.add(new Horse());
+        }
+
+        for (Horse h : stable2) {
+            Froilan.getFroilan().mount(h);
+            System.out.println("FROILAN!");
+            Froilan.getFroilan().dismount(h);
+        }
+    }
+
+    @Test
+    public void rideHorsesStableThree() {
+
+        Stable stable3 = new Stable();
+
+        for (int i = 0; i < 4; i++) {
+            stable3.add(new Horse());
+        }
+
+        for (Horse h : stable3) {
+            Froilan.getFroilan().mount(h);
+            System.out.println("WUBALUBBADUBDUB!");
+            Froilan.getFroilan().dismount(h);
+        }
+    }
+
+}
